d8f9c938adb87f54cbd2bb072934d8bf583f0950
LolRB <rodrigo_luuu@hotmail.com>
2024-09-06 14:18:18 -0600

    Thought model

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+import reactionSchema from "./Reaction.js";
+import dateFormat from "../utils/dateFormat.js";
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: [true, "A thought is required!"],
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    versionKey: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions?.length || 0;
+});
+
+export default model("Thought", thoughtSchema);
